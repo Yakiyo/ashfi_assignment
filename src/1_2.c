75d5359842c9a7492bbfd78442c389392a1c5cdc
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2023-03-18 19:29:49 +0600

    feat: 1_2

---
+++ src/1_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    printf("Provide a number: ");
+    scanf("%d", &n);
+    int len;
+    if (n == 0) {
+        len = 1;
+    } else {
+        // length of an int: https://stackoverflow.com/a/3068420/17990034
+        len = floor(log10(abs(n))) + 1;
+    }
+    int sum = 0;
+    int init = n;
+    do {
+        int digit = n % 10;
+        sum += pow(digit, len);
+        n /= 10;
+    } while (n != 0);
+    if (sum == init) {
+        printf("It is an armstrong number");
+    } else {
+        printf("It is not an armstrong number");
+    }
+    return 0;
+}
